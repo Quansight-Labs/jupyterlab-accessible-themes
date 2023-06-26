14f3686e7fe7a5a22c89eeb4fa5eb3ebe24123e6
Stephannie Jimenez <steff456@hotmail.com>
2023-06-26 18:33:18 -0500

    Release v0.1.1

--- setup.py
+++ setup.py
@@ -2,10 +2,16 @@
 # Distributed under the terms of the Modified BSD License.
 
 import json
-import re
+import os
+import setuptools
 from pathlib import Path
 
-import setuptools
+
+def get_long_description():
+    with open(os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'README.md'
+    ), encoding='utf8') as fp:
+        return fp.read()
 
 HERE = Path(__file__).parent
 MOD = "jupyterlab_accessible_themes"
@@ -30,17 +36,22 @@ for ext, pkg in zip(EXTS, PACKAGES):
 
 DATA_FILES = sorted([(k, v) for k, v in EXT_FILES.items()])
 
+pkg_name = HERE / "package.json"
+PKG_INFO = json.loads(pkg_name.read_text(encoding="utf-8"))
+
 SETUP_ARGS = dict(
-    name=PACKAGES[0]["jupyterlab"]["discovery"]["server"]["base"]["name"],
-    description=PACKAGES[0]["description"],
-    version=PACKAGES[0]["version"],
-    url=PACKAGES[0]["homepage"],
-    license=PACKAGES[0]["license"],
+    name=PKG_INFO["name"],
+    description=PKG_INFO["description"],
+    version=PKG_INFO["version"],
+    url=PKG_INFO["homepage"],
+    license=PKG_INFO["license"],
     packages=["jupyterlab_accessible_themes"],
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
     data_files=DATA_FILES,
     project_urls={
-        "Bug Tracker": PACKAGES[0]["bugs"]["url"],
-        "Source Code": PACKAGES[0]["repository"]["url"],
+        "Bug Tracker": PKG_INFO["bugs"]["url"],
+        "Source Code": PKG_INFO["repository"]["url"],
     },
     author="quansight-labs",
     author_email="contact@quansight.com",
